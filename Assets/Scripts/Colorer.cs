fcd79909a4d7fc4c9346849599c5980a5db4064b
moriiiiiiiiiiii12 <moriiiiiiiiiiii12@gmail.com>
2025-03-21 08:38:36 +0300

    edit colorer

--- Assets/Scripts/Colorer.cs
+++ Assets/Scripts/Colorer.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Colorise : MonoBehaviour
+public class Colorer : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
